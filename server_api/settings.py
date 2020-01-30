fead2c62d8c40d335c9ce57cd923651ee156eb7a
roger <luojie@hnu.edu.cn>
2020-01-30 18:10:36 +0800

    add mysql config

--- server_api/settings.py
+++ server_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '(yq5b9a71*s(+0jba$n^l#-r&40-o0o7d5p#*x0)--g)6(c7m='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*', ]
 
 # Application definition
 
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'server_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wuhan',
+        'PORT': '3306',
+        'HOST': '121.43.233.66',
+        'USER': 'luojie',
+        'PASSWORD': '123456',
     }
 }
 
